22bf3a305293c7e8666214d3943b8949b073ca6d
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-04-27 21:53:21 +0300

    added tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = galleryItems
+    .map(
+        item =>
+            `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+    </a>
+</div>`
+    )
+    .join('');
+
+const closeOnEscape = e => {
+    e.preventDefault();
+    if (e.key === 'Escape') {
+        instance.close();
+    }
+};
+
+const instance = basicLightbox.create(`<img src="" width="800" height="600">`, {
+    onShow: () => {
+        document.addEventListener('keydown', closeOnEscape);
+    },
+    onClose: () => {
+        document.removeEventListener('keydown', closeOnEscape);
+    },
+});
+
+const instanceImageEl = instance.element().querySelector('img');
+
+galleryEl.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    instanceImageEl.src = e.target.dataset.source;
+    instance.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = galleryItems
+    .map(
+        item =>
+            `<div class="gallery__item">
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </div>`
+    )
+    .join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
